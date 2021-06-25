388f109eaf0385eae326cd616265b8cb0879bb5d
Addison <addison.send@gmail.com>
2021-06-25 19:20:07 -0300

    Game basics
    added 2 assets from store and first character logics.

---
+++ The Game/Assets/TrapLogic.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapLogic : MonoBehaviour
+{
+    public BoxCollider2D trap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trap.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
