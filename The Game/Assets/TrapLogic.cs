6322b88fef5204568cfa739660328db267f29e7a
Uarlley do Nascimento Amorim <51249018+iHateAnimes@users.noreply.github.com>
2021-06-26 09:15:07 -0300

    Code fix

--- The Game/Assets/TrapLogic.cs
+++ The Game/Assets/TrapLogic.cs
@@ -8,7 +8,7 @@ public class TrapLogic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        trap.
+      
     }
 
     // Update is called once per frame
