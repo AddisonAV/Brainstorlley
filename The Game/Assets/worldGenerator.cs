6fb4c7239a6ac45e016b9ce61de876bfa6dd4ef5
Uarlley do Nascimento Amorim <51249018+iHateAnimes@users.noreply.github.com>
2021-07-10 16:45:26 -0300

    world generator updated

--- The Game/Assets/worldGenerator.cs
+++ The Game/Assets/worldGenerator.cs
@@ -9,39 +9,40 @@ public class worldGenerator : MonoBehaviour
     public GameObject trap;
     public GameObject ground;
     public GameObject bottonWall;
-
     public GameObject[] enviroment = new GameObject[6];
 
-    private int TABLESIZE = 100;
+    public int fieldWidth = 15;
+    public int fieldHeight = 5;
+    public int difficulty = 2;
 
     Vector3 offset;
     // Start is called before the first frame update
     void Start(){
-
+        //enviroment = new GameObject[enviromentVariety];
         int toSpawn;
         int i;
         offset*=0;
         offset[1] -= 2f;
         offset[0] -= 1.5f;
-        for(i = 0; i < Math.Sqrt(TABLESIZE)+2; i++)
+        for(i = 0; i < fieldHeight+2; i++)
         {
             offset[1] += 1.5f;
             Instantiate(wall, offset, wall.transform.rotation);
         }
 
-        for (i = 0; i < Math.Sqrt(TABLESIZE) + 1; i++)
+        for (i = 0; i < fieldWidth + 1; i++)
         {
             offset[0] += 1.5f;
             Instantiate(wall, offset, wall.transform.rotation);
         }
 
-        for (i = 0; i < Math.Sqrt(TABLESIZE) + 1; i++)
+        for (i = 0; i < fieldHeight + 1; i++)
         {
             offset[1] -= 1.5f;
             Instantiate(wall, offset, wall.transform.rotation);
         }
         offset[1] += 0.5f;
-        for (i = 0; i < Math.Round(Math.Sqrt(TABLESIZE)); i++)
+        for (i = 0; i < fieldWidth; i++)
         {
             offset[0] -= 1.5f;
             Instantiate(bottonWall, offset, bottonWall.transform.rotation);
@@ -50,46 +51,49 @@ public class worldGenerator : MonoBehaviour
         System.Random random = new System.Random();
         //spawn the enviroment
 
-        int enviromentSize = TABLESIZE * 5;
+        /*int enviromentSize = TABLESIZE * 5;
         int sqrtEnv = (int)(Math.Sqrt(enviromentSize));
-        offset[0] = -12;
-        offset[1] = -12;
+        offset[0] = -17;
+        offset[1] = -17;
         Debug.Log(Math.Sqrt(enviromentSize));
         for (i = 0; i < enviromentSize; i++)
         {
-            toSpawn = random.Next(6);
+            toSpawn = random.Next(enviroment.Length);
 
             if (i % sqrtEnv == 0)
             {
-                offset[0] = -12;
+                offset[0] = -17;
                 offset[1] += 2f;
             }
 
             Instantiate(enviroment[toSpawn], offset, ground.transform.rotation);
             offset[0] += 2f;
 
-        }
+        }*/
 
+        int k = 0;
         offset = ground.transform.position;
-        
-
-
-        for(i = 0; i < TABLESIZE; i++){
-            toSpawn = random.Next(2);
-
-            if (i % (Math.Sqrt(TABLESIZE)) == 0){
-                offset[0] = 0;
-                offset[1] += 1.5f;
+        offset[0] = 0;
+        offset[1] += 1.5f;
+        //offset[1] = 0;
+        for (k = 0; k < fieldHeight; k++){
+            for (i = 0; i < fieldWidth; i++)
+            {
+                toSpawn = random.Next(difficulty);
+                if (toSpawn == 0 || i == 0)
+                {
+                    Instantiate(ground, offset, ground.transform.rotation);
+                    offset[0] += 1.5f;
+                }
+                else
+                {
+                    Instantiate(ground, offset, ground.transform.rotation);
+                    Instantiate(trap, offset, trap.transform.rotation);
+                    offset[0] += 1.5f;
+                }
             }
-            if(toSpawn == 0 || i == 0){
-                Instantiate(ground, offset, ground.transform.rotation);
-                offset[0] += 1.5f;
-            }
-            else{
-                Instantiate(ground, offset, ground.transform.rotation);
-                Instantiate(trap, offset, trap.transform.rotation);
-                offset[0] += 1.5f;
-            }
+            offset[1] += 1.5f;
+            offset[0] = 0;
         }
     }
 
