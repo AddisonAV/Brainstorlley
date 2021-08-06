62f69e7e4c87527b5b9f1d62dd6a98ce149224d6
Addison <addison.send@gmail.com>
2021-08-06 15:45:29 -0300

    fixed movement on Pause

--- The Game/Assets/Script/PlayerMovement.cs
+++ The Game/Assets/Script/PlayerMovement.cs
@@ -21,27 +21,26 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-
-        if (movement.x >= 1) LastDir = 0;
-        else if (movement.x <= -1) LastDir = 1;
-        else if (movement.y >= 1) LastDir = 2;
-        else if (movement.y <= -1) LastDir = 3;
-
-        animator.SetInteger("LastDir", LastDir);
-
-    }
-
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * curMoveSpeed * Time.fixedDeltaTime);
+        if (curMoveSpeed > 0)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+
+            if (movement.x >= 1) LastDir = 0;
+            else if (movement.x <= -1) LastDir = 1;
+            else if (movement.y >= 1) LastDir = 2;
+            else if (movement.y <= -1) LastDir = 3;
+
+            animator.SetInteger("LastDir", LastDir);
+
+            rb.MovePosition(rb.position + movement * curMoveSpeed * Time.fixedDeltaTime);
+        }
     }
 
     public void changeMoveSpeed(float newMoveSpeed)
